8121b5c3cfa3d87432366012a55405c2736e4d59
pinejh <pinejh95@gmail.com>
2017-03-29 23:04:25 -0400

    deck functions should be working(havent tested yet)

--- blackjack/cards.go
+++ blackjack/cards.go
@@ -1,6 +1,10 @@
 package main
 
-import sf "github.com/zyedidia/sfml/v2.3/sfml"
+import (
+	"github.com/pinejh/console"
+
+	sf "github.com/zyedidia/sfml/v2.3/sfml"
+)
 
 type Card struct {
 	*sf.RectangleShape
@@ -11,7 +15,7 @@ type Card struct {
 	dispsuit *sf.Sprite
 }
 
-type deck []Card
+type Deck []*Card
 
 func NewCard(num int, suit string) *Card {
 	card := new(Card)
@@ -20,7 +24,8 @@ func NewCard(num int, suit string) *Card {
 	card.SetFillColor(sf.ColorWhite)
 	card.SetOutlineColor(sf.ColorWhite)
 	card.SetOutlineThickness(5)
-	card.show = false
+	card.show = true
+	card.Flip()
 	card.number = num
 	if num == 1 {
 		card.dispnum = "A"
@@ -51,5 +56,44 @@ func (c *Card) Flip() {
 	} else {
 		c.SetFillColor(sf.Color{210, 60, 60, 255})
 	}
+}
 
+func NewDeck() *Deck {
+	deck := make(Deck, 0)
+	for s := 1; s <= 4; s++ {
+		for n := 1; n <= 13; n++ {
+			suit := ""
+			switch s {
+			case 1:
+				suit = "spades"
+			case 2:
+				suit = "diamonds"
+			case 3:
+				suit = "clubs"
+			case 4:
+				suit = "hearts"
+			}
+			deck = append(deck, NewCard(n, suit))
+		}
+	}
+	return &deck
+}
+
+func (d *Deck) Length() int {
+	return len(*d)
+}
+
+func (d *Deck) pickRandom() *Card {
+	i := console.RandInt(0, d.Length())
+	c := (*d)[i]
+	(*d) = append((*d)[:i], (*d)[i+1:]...)
+	return c
+}
+
+func (d *Deck) Shuffle() {
+	deck := make(Deck, 0)
+	for d.Length() > 0 {
+		deck = append(deck, d.pickRandom())
+	}
+	d = &deck
 }
