4fc18c1e8d52267c1f6bb34dc2146edb49e376ad
pinejh <pinejh95@gmail.com>
2017-04-20 12:29:47 -0400

    deleted functions

--- spaceshooter/asteroid.go
+++ spaceshooter/asteroid.go
@@ -41,50 +41,8 @@ func GenAsteroid() {
 	ast = append(ast, NewAsteroid(v, rand(-179, 180), rand(3, 6), rand(1, 5)))
 }
 
-/*
-func CloneAsteroid(a *Asteroid) *Asteroid {
-	ast := new(Asteroid)
-	ast.Sprite = a.Sprite
-	ast.SetPosition(a.GetPosition())
-	ast.SetRotation(a.GetRotation())
-	ast.width = a.width
-	ast.height = a.height
-	ast.rotationSpeed = a.rotationSpeed
-	ast.vel = a.vel
-	ast.dead = a.dead
-	return ast
-}
-func CloneAsteroidAt(a *Asteroid, pos sf.Vector2f) {
-	asteroid := CloneAsteroid(a)
-	asteroid.SetPosition(pos)
-	ast = append(ast, asteroid)
-}
-*/
-
 func (a *Asteroid) Update(dt float32) {
 	a.Rotate(a.rotationSpeed)
 	a.Move(sf.Vector2f{a.vel.X * dt, a.vel.Y})
 	Wrap(a.Sprite)
 }
-
-/*
-func (a *Asteroid) Wrap() {
-	v := a.GetPosition()
-	width, height := a.GetGlobalBounds().Width, a.GetGlobalBounds().Height
-	if v.X+width/2 < 0 {
-		CloneAsteroidAt(a, sf.Vector2f{screenWidth + width/2, v.Y})
-	}
-	if v.X-width/2 > screenWidth {
-		CloneAsteroidAt(a, sf.Vector2f{-width / 2, v.Y})
-	}
-	if v.Y+height/2 < 0 {
-		CloneAsteroidAt(a, sf.Vector2f{v.X, screenHeight + height/2})
-	}
-	if v.Y-height/2 > screenHeight {
-		CloneAsteroidAt(a, sf.Vector2f{v.X, -height / 2})
-	}
-	if v.X+width/2 < 0 || v.X-width/2 > screenWidth || v.Y+height/2 < 0 || v.Y-height/2 > screenHeight {
-		a.dead = true
-	}
-}
-*/
